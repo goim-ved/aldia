40c19036bc90c0cf901531cead5911e9e9c4488b
goim-ved <vedantgoim1@gmail.com>
2026-09-11 22:20:57 +0530

    Fix: Add email-validator dependency and set PYTHONPATH for container runtime

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,9 @@
 import asyncio
 from logging.config import fileConfig
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from alembic import context
 from sqlalchemy import pool
